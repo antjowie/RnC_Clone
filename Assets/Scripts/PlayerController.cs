26e675f6eae450632fc24f262fd5cc1d634d9334
Angelo <7332321+antjowie@users.noreply.github.com>
2020-05-13 12:33:39 +0200

    Character now moves in direction of camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 100;
-    
+    public CinemachineVirtualCameraBase playerCamera;
+
     Rigidbody rb;
 
     void Awake()
@@ -18,8 +20,14 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        rb.AddForce(transform.forward * y * moveSpeed * Time.deltaTime);
-        rb.AddForce(transform.right * x * moveSpeed * Time.deltaTime);
+        // Calculate intended movement via the player view direction
+        var forward = playerCamera.LookAt.position - playerCamera.transform.position;
+        forward.y = 0;
+        forward.Normalize();
+        var right = Vector3.Cross(transform.up, forward);
+
+        rb.AddForce(forward * y * moveSpeed * Time.deltaTime);
+        rb.AddForce(right * x * moveSpeed * Time.deltaTime);
 
         // Should be force based
         //rb.MovePosition(rb.transform.position + new Vector3(x, 0, y) * Time.deltaTime * moveSpeed);
